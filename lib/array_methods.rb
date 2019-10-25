e09545381c34592bcdeb3c594498ca0c475d8daf
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-25 02:26:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -26,13 +26,11 @@ end
 
 
 def find_element_index(array, value_to_find)
-  count = 0
-
-while count < array.length do 
+ 
+ array.length.times do [count]
  if array[count] == value_to_find
-  return count 
-end 
-count += 1
-end 
-nil 
+   return count 
+ end 
+ end 
+nil
 end 
